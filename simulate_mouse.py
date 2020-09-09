4948b1236ae6bff1f3128035a5914f0ac3959b56
Max <Max.Kemmerer@gmx.net>
2020-09-09 12:55:40 +0200

    added padding

--- simulate_mouse.py
+++ simulate_mouse.py
@@ -6,7 +6,7 @@ import time
 duration_range = (1, 100)
 
 # defines the min/max duration the script should pause between mouse moves
-sleep_range = (20, 300)
+sleep_range = (20, 100)
 
 # get screen dimensions
 display_width, display_height = pyautogui.size()
@@ -15,7 +15,10 @@ display_width, display_height = pyautogui.size()
 # simulate mouse movement randomly across the screen
 def simulate(width=display_width, height=display_height, duration=duration_range, sleep = sleep_range):
     while True:
-        pyautogui.moveTo(random.randint(0, width), random.randint(0, height), random.randint(duration[0], duration[1]))
+        # adjust screen - 10% padding
+        pyautogui.moveTo(random.randint(width*0.1, width*0.9),
+                         random.randint(height*0.1, height*0.9),
+                         random.randint(duration[0], duration[1])/10)
         time.sleep(random.randint(sleep[0], sleep[1]))
 
 
